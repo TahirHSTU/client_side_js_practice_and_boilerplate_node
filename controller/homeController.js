66a0c507cf575e49bd2b8f3896af73714abeb751
TahirHSTU <tahirtamin20@gmail.com>
2021-10-01 23:37:58 +0600

    added basic MVC format

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,4 +1,5 @@
 const {app, ejs, express} = require('../dependencies')
+const Home = require('../model/homeModel')
 
 exports.homeFunction = function(req, res){
     res.render('home')
---
+++ model/homeModel.js
@@ -0,0 +1,6 @@
+let Home =  function(data){
+    this.data =data
+
+}
+
+module.exports = Home
